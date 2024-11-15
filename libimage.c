a0d9e36dbb3c8ae0fca2de90d51860ead0250c26
RasdaCorentin <94112002+RasdaCorentin@users.noreply.github.com>
2024-11-15 16:25:40 +0400

    Add files via upload

--- libimage.c
+++ libimage.c
@@ -4,15 +4,115 @@
 
 #include "libimage.h"
 
+//#########################################################################################################
+//#######################################         IMP2         ############################################
+//#########################################################################################################
+ImP2 initImP2(int haut, int larg, int vmax) {
+    ImP2 InitImP2;
+    int i;
+    InitImP2.hauteur = haut;
+    InitImP2.largeur = larg;
+    InitImP2.maxval = vmax;
+    InitImP2.tpix = malloc(haut * sizeof(unsigned char *));
+    for(i = 0; i<InitImP2.hauteur; i++){
+        InitImP2.tpix[i] = malloc(larg * sizeof(unsigned char));
+    }
+    return InitImP2;
+}
+
+ImP2 copieImP2(ImP2 im) {
+    ImP2 newImP2;
+    
+    int i,j;
+    newImP2 = initImP2(im.hauteur, im.largeur, im.maxval);
+    for(i=0; i<newImP2.hauteur*newImP2.largeur; i++){
+        newImP2.tpix[i][j] = im.tpix[i][j];
+    }
+    return newImP2;
+}
+
+ImP2 chargeImP2(char* fichier){
+
+    FILE* fsource = fopen(fichier, "r");
+    int hauteur, largeur, vmax, tmax;
+    int i= 0, j = 0, k =0;
+    char type[3];
+    char lecture[255];
+
+    ImP2 newImP2;
+    if (fsource == NULL) {
+        fprintf(stderr, "Erreur lors de l'ouverture du fichier. %s introuvable", fichier);
+        return newImP2;
+    }
+
+    fscanf(fsource, "%s", type);
+    if (strcmp(type, "P2") != 0) {
+        fprintf(stderr, "Erreur de typage \"%s\" incompatible.", type);
+        return newImP2;
+    }
+
+    //Cette boucle permet de vérifie rsi le debut de la ligne est un "#"
+    //On ne la traite pas et utilise fgets pour passer à la prochaine ligne
+    while((fscanf(fsource,"%s",lecture)) != EOF) {
+        if (lecture[0] == '#'){
+            fgets(lecture,sizeof(lecture), fsource);
+        }
+        else break;
+    }
+    
+    hauteur = atoi(lecture);
+    fscanf(fsource, "%s", lecture);
+    largeur = atoi(lecture);
+    fscanf(fsource, "%s", lecture);
+    vmax = atoi(lecture);
+
+    newImP2 = initImP2(hauteur, largeur, vmax);
+
+    newImP2.hauteur = hauteur;
+    newImP2.largeur = largeur;
+    newImP2.maxval = vmax;
+
+    for(j=0; j<newImP2.hauteur; j++){
+        for(k=0; k<newImP2.largeur; k++){
+        fscanf(fsource, "%hhu", &newImP2.tpix[j][k]);
+    }}
+    return newImP2;
+}
+void sauveImP2(char* nom, ImP2 im){
+    int i, j;
+    FILE * fdest = fopen(nom, "w"); // Ouverture du fichier destination en écriture et vérification
+    if (fdest == NULL){
+        fprintf(stderr, "\n%s : erreur à la creation du fichier", nom);
+        abort();
+    }
+
+    //Mettre Type
+    fprintf(fdest, "P2\n");
+    //Mettre Commentaire 
+    fprintf(fdest, "#RASDA_RAZAFITSALAMA ANDRIAMAHENINA_43004877_36000793\n");
+    //Mettre  Mettre Hauteur Largeur
+    fprintf(fdest, "%d %d\n", im.hauteur, im.largeur);
+    //Mettre Vmax
+    fprintf(fdest, "%d\n", im.maxval);
+
+    for(i=0; i<im.hauteur; i++){
+        for(j=0; j<im.largeur; j++){
+        fprintf(fdest, "%hhu\n", im.tpix[i][j]);
+    }
+    }
+}
+//#########################################################################################################
+//#######################################         IMP3         ############################################
+//#########################################################################################################
 ImP3 initImP3(int haut, int larg, int vmax) {
     ImP3 InitImP3;
     int i;
     InitImP3.hauteur = haut;
     InitImP3.largeur = larg;
     InitImP3.maxval = vmax;
-    InitImP3.tpix = malloc(haut * sizeof(int *));
+    InitImP3.tpix = malloc(haut * sizeof(PixelRGB *));
     for(i=0; i<haut; i++){
-        InitImP3.tpix[i]= malloc(larg * sizeof(int));
+        InitImP3.tpix[i]= malloc(larg * sizeof(PixelRGB));
     }
     return InitImP3;
 }
@@ -30,23 +130,28 @@ ImP3 copieImP3(ImP3 im) {
 }
 
 //Manque "si le type de l’image contenu dans le fichier n’est pas P3"
-//charge image est simplifiable TABD INUTILE cf: ICI
 ImP3 chargeImP3(char* fichier){
+
     FILE* fsource = fopen(fichier, "r");
     int hauteur, largeur, vmax, tmax;
-    PixelRGB** tabD;
     int i= 0, j = 0, k =0;
-    char type[2];
+    char type[3];
     char lecture[255];
-    int checkC = 0;
 
     ImP3 newImP3;
     if (fsource == NULL) {
-        fscanf(stderr, "Erreur lors de l'ouverture du fichier");
+        fprintf(stderr, "Erreur lors de l'ouverture du fichier. %s introuvable", fichier);
         return newImP3;
     }
 
     fscanf(fsource, "%s", type);
+    if (strcmp(type, "P3") != 0) {
+        fprintf(stderr, "Erreur de typage \"%s\" incompatible.", type);
+        return newImP3;
+    }
+
+    //Cette boucle permet de vérifie rsi le debut de la ligne est un "#"
+    //On ne la traite pas et utilise fgets pour passer à la prochaine ligne
     while((fscanf(fsource,"%s",lecture)) != EOF) {
         if (lecture[0] == '#'){
             fgets(lecture,sizeof(lecture), fsource);
@@ -58,14 +163,7 @@ ImP3 chargeImP3(char* fichier){
     fscanf(fsource, "%s", lecture);
     largeur = atoi(lecture);
     fscanf(fsource, "%s", lecture);
-    vmax= atoi(lecture);
-
-    tabD = malloc(((hauteur*largeur)) * sizeof(PixelRGB));
-    if (tabD == NULL) {
-        fprintf(stderr,"Erreur d'allocation mémoire");
-        fclose(fsource);
-        return newImP3;
-    } 
+    vmax = atoi(lecture);
 
     newImP3 = initImP3(hauteur, largeur, vmax);
 
@@ -77,7 +175,6 @@ ImP3 chargeImP3(char* fichier){
         for(k=0; k<newImP3.largeur; k++){
         fscanf(fsource, "%hhu %hhu %hhu", &newImP3.tpix[j][k].r, &newImP3.tpix[j][k].v, &newImP3.tpix[j][k].b);
     }}
-    free(tabD);
     return newImP3;
 }
 
@@ -85,21 +182,31 @@ void sauveImP3(char* nom, ImP3 im){
     int i, j;
     FILE * fdest = fopen(nom, "w"); // Ouverture du fichier destination en écriture et vérification
     if (fdest == NULL){
-        fprintf(stderr, "\n%s : erreur à la création du fichier", nom);
+        fprintf(stderr, "\n%s : erreur à la creation du fichier", nom);
         abort();
     }
+
+    //Mettre Type
+    fprintf(fdest, "P3\n");
+    //Mettre Commentaire 
+    fprintf(fdest, "#RASDA_RAZAFITSALAMA ANDRIAMAHENINA_43004877_36000793\n");
+    //Mettre  Mettre Hauteur Largeur
+    fprintf(fdest, "%d %d\n", im.hauteur, im.largeur);
+    //Mettre Vmax
+    fprintf(fdest, "%d\n", im.maxval);
+
     for(i=0; i<im.hauteur; i++){
         for(j=0; j<im.largeur; j++){
         fprintf(fdest, "%hhu\n%hhu\n%hhu\n", im.tpix[i][j].r, im.tpix[i][j].v, im.tpix[i][j].b);
     }
-    
 }
+
 }
 
 int main(){
-    char* fichier = "./CCTP24/raki.ppm";
-    char* nom = "./CCTP24/raki2.ppm";
-    ImP3 oldImP3, newImP3; 
-    oldImP3 = chargeImP3(fichier);
-    sauveImP3(nom, oldImP3);
+    char* fichier = "./CCTP24/chaises.pgm";
+    char* nom = "./CCTP24/chaises2.pgm";
+    ImP2 oldImP2, newImP2; 
+    oldImP2 = chargeImP2(fichier);
+    sauveImP2(nom, oldImP2);
 }
--- libimage.h
+++ libimage.h
@@ -33,25 +33,47 @@ typedef struct image2 {
 } ImP2;
 
 // initImP3 : int x int x int -> ImP3
-// Entrée : 3 entiers, hauteur , largeur et vmax de l'image
-// Sortie : une structure ImP3 (tableau de tableau)
+// Entrée : 3 entiers, hauteur , largeur et la valeur max de l'image (intensité de couleur)
+// Sortie : une structure ImP3 initialisé avec les valeurs passee et le malloc de tpix déjà fait
 extern ImP3 initImP3(int haut, int larg, int vmax);
 
 // copieImP3 : ImP3 -> ImP3
-// Entrée : un ImP3 im
+// Entrée : une ImP3 a copié
 // Sortie : une copie de la structure ImP3 entrée en paramètre
 extern ImP3 copieImP3(ImP3 im);
 
 // chargeImP3 : char* -> ImP3
 // Entrée : un pointeur sur le nom du fichier à charger
-// Sortie : une structure ImP3 avec les valeurs dicté par le fichier.
+// Sortie : une structure ImP3 avec les valeurs dictée par le fichier.
 extern ImP3 chargeImP3(char* fichier);
 
 
-// chargeImP3 : char* x ImP3 -> ImP3
-// Entrée : un pointeur sur une chaine de caractère et une image
+// sauveImP3 : char* x ImP3 -> ImP3
+// Entrée : le nom du fichier a copier et une image
 // Sortie : Rien
 //Crée un fichier image .ppm
 extern void sauveImP3(char* nom, ImP3 im);
+
+// initImP2 : int x int x int -> ImP2
+// Entrée : 3 entiers, hauteur , largeur et la valeur max de l'image (intensité de couleur)
+// Sortie : une structure ImP3 initialisé avec les valeurs passee et le malloc de tpix déjà fait
+extern ImP2 initImP2(int haut, int larg, int vmax);
+
+// copieImP2 : ImP2 -> ImP2
+// Entrée : un ImP2 im
+// Sortie : une copie de la structure ImP2 entrée en paramètre
+extern ImP2 copieImP2(ImP2 im);
+
+// chargeImP2 : char* -> ImP2
+// Entrée : un pointeur sur le nom du fichier à charger
+// Sortie : une structure ImP2 avec les valeurs dicté par le fichier.
+extern ImP2 chargeImP2(char* fichier);
+
+
+// chargeImP2 : char* x ImP2 -> ImP2
+// Entrée : le nom du fichier a copier et une image
+// Sortie : Rien
+//Crée un fichier image .ppm
+extern void sauveImP2(char* nom, ImP2 im);
 
 #endif
